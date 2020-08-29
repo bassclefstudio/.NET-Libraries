8dd2c697e3732468e1a47600e816c617d01ef78f
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-08-29 16:29:34 -0400

    Added the IIdentifiable interface to .NET lib.

---
+++ BassClefStudio.NET.Core/IIdentifiable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BassClefStudio.NET.Core
+{
+    /// <summary>
+    /// Represents an object that can be identified by a <typeparamref name="T"/> key.
+    /// </summary>
+    /// <typeparam name="T">The type of key.</typeparam>
+    public interface IIdentifiable<T>
+    {
+        /// <summary>
+        /// The (usually unique) <typeparamref name="T"/> key of the <see cref="IIdentifiable{T}"/> object.
+        /// </summary>
+        T Id { get; }
+    }
+}
