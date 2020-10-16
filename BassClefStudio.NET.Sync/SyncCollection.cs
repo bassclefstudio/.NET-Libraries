2f4c647957298659c987f7860eecb4520b523abe
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-10-16 13:04:55 -0400

    Removed unnecessary generic parameter from SyncCollection<TItem, TKey> and added INotifyPropertyChanged requirement to ISyncItem<T>.

--- BassClefStudio.NET.Sync/SyncCollection.cs
+++ BassClefStudio.NET.Sync/SyncCollection.cs
@@ -23,25 +23,24 @@ namespace BassClefStudio.NET.Sync
     /// <summary>
     /// Represents a synced keyed <see cref="ISyncCollection{T}"/> of <see cref="ISyncItem{T}"/>s of type <typeparamref name="TItem"/>.
     /// </summary>
-    /// <typeparam name="T">The type of items in the collection. This must inherit from <see cref="IKeyedSyncItem{T, TKey}"/> to support required syncing of items and getting/setting by key.</typeparam>
     /// <typeparam name="TItem">The type of items in the collection.</typeparam>
     /// <typeparam name="TKey">The type of the key used to sync each item in the collection.</typeparam>
-    public abstract class SyncCollection<T, TItem, TKey> : Observable, ISyncCollection<T> where T : IKeyedSyncItem<TItem, TKey> where TItem : IIdentifiable<TKey> where TKey : IEquatable<TKey>
+    public abstract class SyncCollection<TItem, TKey> : Observable, ISyncCollection<IKeyedSyncItem<TItem, TKey>> where TItem : IIdentifiable<TKey> where TKey : IEquatable<TKey>
     {
-        private ObservableCollection<T> item;
+        private ObservableCollection<IKeyedSyncItem<TItem, TKey>> item;
         /// <inheritdoc/>
-        public ObservableCollection<T> Item { get => item; set => Set(ref item, value); }
+        public ObservableCollection<IKeyedSyncItem<TItem, TKey>> Item { get => item; set => Set(ref item, value); }
 
         private bool isLoading;
         /// <inheritdoc/>
         public bool IsLoading { get => isLoading; set => Set(ref isLoading, value); }
 
         /// <summary>
-        /// Creates a new empty <see cref="SyncCollection{T, TItem, TKey}"/>.
+        /// Creates a new empty <see cref="SyncCollection{TItem, TKey}"/>.
         /// </summary>
         public SyncCollection()
         {
-            Item = new ObservableCollection<T>();
+            Item = new ObservableCollection<IKeyedSyncItem<TItem, TKey>>();
         }
 
         /// <summary>
@@ -56,19 +55,19 @@ namespace BassClefStudio.NET.Sync
         protected abstract Task<ISyncCollectionInfo<TItem, TKey>> GetCollectionInfo();
 
         /// <summary>
-        /// Creates a <typeparamref name="T"/> item to populate a new item in the collection.
+        /// Creates an <see cref="IKeyedSyncItem{TItem, TKey}"/> item to populate a new item in the collection.
         /// </summary>
         /// <param name="link">The <see cref="ILink{T}"/> created for the syncing of the item.</param>
-        protected abstract T CreateSyncItem(ILink<TItem> link);
+        protected abstract IKeyedSyncItem<TItem, TKey> CreateSyncItem(ILink<TItem> link);
 
         /// <inheritdoc/>
-        public async Task UpdateAsync(ISyncInfo<ObservableCollection<T>> info = null)
+        public async Task UpdateAsync(ISyncInfo<ObservableCollection<IKeyedSyncItem<TItem, TKey>>> info = null)
         {
             IsLoading = true;
             var collectionInfo = await GetCollectionInfo();
             if(Item == null)
             {
-                Item = new ObservableCollection<T>();
+                Item = new ObservableCollection<IKeyedSyncItem<TItem, TKey>>();
             }
             Item.Sync(collectionInfo.GetKeys(), i => CreateSyncItem(GetLink(i)));
             await Item.Select(i => i.UpdateAsync(collectionInfo)).RunParallelAsync();
@@ -76,7 +75,7 @@ namespace BassClefStudio.NET.Sync
         }
 
         /// <inheritdoc/>
-        public async Task PushAsync(ISyncInfo<ObservableCollection<T>> info = null)
+        public async Task PushAsync(ISyncInfo<ObservableCollection<IKeyedSyncItem<TItem, TKey>>> info = null)
         {
             IsLoading = true;
             if (info is ISyncInfo<TItem> syncInfo)
